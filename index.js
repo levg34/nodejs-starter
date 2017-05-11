4ba00efdd10e1c1270b2e7fca4477d1abf8387c7
levg34 <levg34@users.noreply.github.com>
2017-05-11 10:18:06 +0200

    complete template

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 var express = require('express')
 var app = express()
+var server = require('http').createServer(app)
+
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', function (req, res) {
-	res.send(JSON.stringify({}))
+	res.sendFile(__dirname + '/view/index.html')
 })
 
-app.listen(server_port, function () {
+app.get('/json', function (req, res) {
+	res.send(JSON.stringify({json:true}))
+})
+
+server.listen(server_port,server_ip_address,function () {
 	console.log("Listening on " + server_ip_address + ", port " + server_port)
 })
